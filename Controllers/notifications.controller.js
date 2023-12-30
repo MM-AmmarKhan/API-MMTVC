5cb3ed0bf02478e53aabdce8794447f1d01fd11f
MAmmarKhan <128587446+MM-AmmarKhan@users.noreply.github.com>
2023-12-30 14:20:05 +0500

    Update notifications.controller.js

--- Controllers/notifications.controller.js
+++ Controllers/notifications.controller.js
@@ -1,27 +1,32 @@
 const db = require("../index");
 
 exports.tvcalert = async (req, res) => {
-    const {message, fcm_id} = req.body;
-    if(!message || !fcm_id) return res.status(400).send({ message: "Missing Message or FCM_ID" });
-    if (fcm_id.length > 5 && message && message.length > 0) {    
-      const data = {
-        to: fcm_id,
-        notification: {
-          body: message,
-        },
-      };  
-      const headers = {
-        'Authorization': 'key=AIzaSyAMJ8jCgs_YVP30M1Kods2Y44DO_iFntFQ', // Replace with your FCM server key
-        'Content-Type': 'application/json',
-      };
-      try {
-        const response = await axios.post('https://fcm.googleapis.com/fcm/send', data, { headers });
-        return res.status(200).send(response.data);
-      } catch (error) {
-        console.error('Error sending FCM notification:', error.message);
-        return res.status(500).send('Internal Server Error');
-      }
-    } else {
-      return res.status(400).send('Invalid Request');
-    }
+  const { jobid, message, fcm_id } = req.body;
+  if (!jobid || !message || !fcm_id) return res.status(400).send({ message: "Missing Message or FCM_ID" });
+  const data = {
+    to: fcm_id,
+    notification: {
+      body: message,
+    },
   };
+  const headers = {
+    'Authorization': 'key=AIzaSyAMJ8jCgs_YVP30M1Kods2Y44DO_iFntFQ', // Replace with your FCM server key
+    'Content-Type': 'application/json',
+  };
+  try {
+    const response = await axios.post('https://fcm.googleapis.com/fcm/send', data, { headers });
+    if(response.data.failure > 0) {
+      return res.status(500).send('Internal Server Error');
+    }
+    const result = await db.sequelize.query(`UPDATE phonebook.sms_jobs SET delivered = 1 WHERE jobsID = ` + jobid, { type: db.sequelize.QueryTypes.UPDATE });
+    if(result[1] === 0) {
+      return res.status(208).send('Already Delivered');
+    }
+    else{
+      return res.status(200).send('Delivered');
+    }    
+  } catch (error) {
+    console.error('Error sending FCM notification:', error.message);
+    return res.status(500).send('Internal Server Error');
+  }
+};
